499992191a0e58b3598fcbb16cef6fde8311b723
andrey32741553 <65930776+andrey32741553@users.noreply.github.com>
2020-10-30 21:01:53 +1000

    Удалены комментарии и отладочные принты

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -1,35 +1,22 @@
 from collections import Counter
-
 from django.shortcuts import render
 
-# Для отладки механизма ab-тестирования используйте эти счетчики
-# в качестве хранилища количества показов и количества переходов.
-# но помните, что в реальных проектах так не стоит делать
-# так как при перезапуске приложения они обнулятся
 click_list = []
 show_list = []
 
 
 def index(request):
-    # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
     if request.GET.get('from-landing'):
         click_list.append('from-landing')
-        print(Counter(click_list))
         return render(request, 'index.html')
 
 
 def landing(request):
-    # Реализуйте дополнительное отображение по шаблону app/landing_alternate.html
-    # в зависимости от GET параметра ab-test-arg
-    # который может принимать значения original и test
-    # Так же реализуйте логику подсчета количества показов
     if request.GET.get('ab-test-arg') == 'test':
         show_list.append('landing_alternate.html')
-        print(Counter(show_list))
         return render(request, 'landing_alternate.html')
     elif request.GET.get('ab-test-arg') == 'original':
         show_list.append('landing.html')
-        print(Counter(show_list))
         return render(request, 'landing.html')
 
 
@@ -39,8 +26,6 @@ def stats(request):
         counter_show = Counter(show_list)
         test_conversion = counter_click['from-landing']/counter_show['landing_alternate.html']
         original_conversion = counter_click['from-landing']/counter_show['landing.html']
-        # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
-        # Для вывода результат передайте в следующем формате:
         return render(request, 'stats.html', context={
             'test_conversion': test_conversion,
             'original_conversion': original_conversion,
